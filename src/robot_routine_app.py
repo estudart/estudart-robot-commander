4f0213bc5d9403344c8657cb2403427f5166422f
Studart Erico <erico.studart@bcg.com>
2026-04-27 15:46:45 -0300

    fix: remove unused file

--- src/robot_routine_app.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from src.presentation.dependencies import get_robot_routine
-
-
-if __name__ == "__main__":
-	robot_routine = get_robot_routine()
-	robot_routine.start_loop()
-
